732198d04d13e49e7c5d50c3f6a28b7d4eb3d6e1
disdev <64833787+Devdish@users.noreply.github.com>
2020-07-27 09:41:55 -0500

    Create user.php

---
+++ user.php
@@ -0,0 +1,27 @@
+              <form action="addprocess.php" enctype="multipart/form-data"  method="post">
+                    <div class="col-md-12 px-1">
+                      <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" class="form-control" placeholder="Your Name" name="name">
+                      </div>
+                    </div>
+                    <div class="col-md-12 px-1">
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">Price</label>
+                        <input type="text" class="form-control" placeholder="Enter Price" name="price">
+                      </div>
+                    </div>
+                    <div class="col-md-12 px-1">
+                      <div class="form-group">
+                        <label>Discription</label>
+                        <input type="text" class="form-control" placeholder="Discription" name="description">
+                      </div>
+                    </div>
+                    <div class="col-md-12 px-1">
+                      <div class="form-group">
+                        <label>Add Image</label>
+                        <input type="file" class="form-control" placeholder="Discription" name="description">
+                      </div>
+                    </div>
+                    <button type="submit" name="submit" class="buttoncall">Add Product</button>
+                  </form>
